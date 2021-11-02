2a4ccf6924365ad23aa520b5eaaac17880d87410
huntereureka <53546873+huntereureka@users.noreply.github.com>
2021-11-02 12:07:02 +0800

    test adding uploadfile.js

---
+++ static/scripts/uploadFile.js
@@ -0,0 +1,10 @@
+function load_desc() {
+    alert("test")
+
+    $('.hover_bkgr_fricc').click(function() {
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function() {
+        $('.hover_bkgr_fricc').hide();
+    });
+};
