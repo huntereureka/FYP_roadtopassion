ae96ba260bf4ff9c8a35be13831bb1e9190eced2
huntereureka <53546873+huntereureka@users.noreply.github.com>
2021-11-23 13:53:41 +0800

    test

--- static/scripts/quizMain.js
+++ static/scripts/quizMain.js
@@ -175,9 +175,10 @@ function OnNext() {
     } else if (current_qn == 1) {
         document.getElementById("back").style.visibility = "visible";
     } else if (current_qn == MAX_QN_NUM - 1) { //last question
-        document.getElementById("submit").style.visibility = "visible";
+        document.getElementById("submit").style.visibility = "hidden";
         document.getElementById("next").style.visibility = "hidden";
         document.getElementById("crystal").style.visibility = "visible";
+        document.getElementById("crystal1").style.visibility = "visible";
     }
 }
 
